898d71570b22cfb86bd44076c992f4e3a9832cd3
Fabian Peter Hammerle <fabian@hammerle.me>
2019-05-03 16:58:36 +0200

    extend docstring sample: chain Freesurfer & ASHS find() iterator

--- freesurfer_volume_reader/__init__.py
+++ freesurfer_volume_reader/__init__.py
@@ -1,11 +1,16 @@
 """
-Read hippocampal subfield volumes computed by Freesurfer
+Read hippocampal subfield volumes computed by Freesurfer and/or ASHS
 
+https://sites.google.com/site/hipposubfields/home
 https://surfer.nmr.mgh.harvard.edu/fswiki/HippocampalSubfields
 
->>> from freesurfer_volume_reader.freesurfer import HippocampalSubfieldsVolumeFile
+>>> from freesurfer_volume_reader import ashs, freesurfer
 >>>
->>> for volume_file in HippocampalSubfieldsVolumeFile.find('/my/freesurfer/subjects'):
+>>> for volume_file in itertools.chain(
+>>>         ashs.HippocampalSubfieldsVolumeFile.find('/my/ashs/subjects'),
+>>>         freesurfer.HippocampalSubfieldsVolumeFile.find('/my/freesurfer/subjects')):
+>>>     print(volume_file.absolute_path)
+>>>     print(volume_file.subject, volume_file.hemisphere)
 >>>     print(volume_file.read_volumes_mm3())
 >>>     print(volume_file.read_volumes_dataframe())
 """
